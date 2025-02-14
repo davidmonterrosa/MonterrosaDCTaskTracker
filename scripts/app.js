ad0a3e8ea499ff8b2568fbb7b45efec32ce817db
davidmonterrosa <dmonterrosa99@gmail.com>
2025-02-14 10:35:39 -0800

    Added Readme file

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
 
 // Id Section
 const toDoListArea = document.getElementById("toDoListArea");
+const inProgressListArea = document.getElementById("inProgressListArea");
+const completedListArea = document.getElementById("completedListArea");
